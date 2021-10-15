6952538d55f518757b44f0e60e7ecc5505ffdf2d
shaharyarskipq <shaharyar.s@skipq.org>
2021-10-15 17:05:27 +0000

    Commit after Review

---
+++ shaharyar/infra/cdk.out/asset.97b5f247b9ce218da8fdcd63c8b182f8d44162156f2541400a88ff316937ab94/handler.py
@@ -0,0 +1,25 @@
+# def lambda_handler(event,context):
+    # message = 'Hello {} {}!'.format(event['first_name'],event['last_name'])
+    # return {
+    #     'message':message
+    # }
+import urllib3
+import constants
+def health_web(event,context):
+    url = constants.URLs
+    availability = get_availability(url)
+    # latency = get_latency(url)
+    return {
+        'Availability':availability,
+        'Latency':latency
+    }
+    
+def get_availability(url):
+    # availability = urllib3.get_availability(url)
+    availability =1
+    return availability
+
+def get_latency(url):
+    latency = urllib3.latency(url)
+    latency = 0.3
+    return latency
