cfcb713a4cc9fa058a653f503cdb23f9ca0bc7c6
shazib2021skipq <92203133+shazib2021skipq@users.noreply.github.com>
2021-10-15 17:20:41 +0500

    Update latency_handler.py

--- shazib/sprint1/lambda/latency_handler.py
+++ shazib/sprint1/lambda/latency_handler.py
@@ -3,6 +3,10 @@ import urllib.request
 import time
 
 def handler(event, context):
+    """
+    Returns a dict with latency and availability as keypairs with their corresponding values.
+    
+    """
     url = CONSTANTS.URL
     return dict(
             {
@@ -12,10 +16,27 @@ def handler(event, context):
         )
 
 def get_availability(url: str) -> int:
+    """
+    Returns the availability status of the target site.
+
+        Parameters:
+                url (str): A string containing the url of the site.
+
+        Returns:
+                    (int): A interger, either 1 or 0 based on the conditions
+    """
     return 1 if urllib.request.urlopen(url).getcode() == 200 else 0
 
 def get_latency(url: str) -> float:
+    """
+    Returns the latency (in ms) of the target site.
 
+        Parameters:
+                url (str): A string containing the url.
+
+        Returns:
+                latency (float): Latency in ms
+    """
     # Send a request
     web_request = urllib.request.urlopen(url)
     # Start the timer
